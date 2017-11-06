75e8ec5b7cfcdbad667c69004bdc0db018c6f43b
ignjaticaco <aleksandar.ignjatic99@gmail.com>
2017-11-06 08:28:34 +0100

    j

--- dodajProstor.php
+++ dodajProstor.php
@@ -7,9 +7,4 @@ if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
     echo "Please log in first to see this page."
     . "<a href ='login.php'> Klikni tukaj za prijavo </a> ";
 }
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 

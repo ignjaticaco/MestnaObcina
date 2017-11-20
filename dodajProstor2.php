793680282b7e2d7710c3e98d8a26517a612a3e37
Chill122 <33390684+Chill122@users.noreply.github.com>
2017-11-20 13:54:52 +0100

    Update dodajProstor2.php

--- dodajProstor2.php
+++ dodajProstor2.php
@@ -1,68 +1,63 @@
 <?php
-session_start();
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "mestna_obcina";
-
-if(isset($_FILES['image'])){
-      $errors= array();
-      $file_name = $_FILES['image']['name'];
-      $file_size = $_FILES['image']['size'];
-      $file_tmp = $_FILES['image']['tmp_name'];
-      $file_type = $_FILES['image']['type'];
-      $file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
-      
-      $expensions= array("jpeg","jpg","png");
-      
-      if(in_array($file_ext,$expensions)=== false){
-         $errors[]="extension not allowed, please choose a JPEG or PNG file.";
-      }
-      
-      if($file_size > 2097152) {
-         $errors[]='File size must be excately 2 MB';
-      }
-      
-      if(empty($errors)==true) {
-         move_uploaded_file($file_tmp,"images/".$file_name);
-         echo "Success";
-      }else{
-         print_r($errors);
-      }
-   
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-
-$stanje = $_POST['stanje'];
-$velikost = $_POST['velikost'];
-$lokacija = $_POST['lokacija'];
-$najemnina = $_POST['najemnina'];
-$opis = $_POST['opis'];
-$zacetek = $_POST['zacetek'];
-$konec = $_POST['konec'];
-$pot = $_POST ['image'];
-$ime = $_FILES ['image'] ["name"];
-$mysql_path = $pot."".$ime;
+require_once 'db.php';
+$target_dir = "slike/";
+$file = $_FILES["fileToUpload"]["name"];
+$target_file = $target_dir . $file;
+$uploadOk = 1;
+$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+}
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $uploadOk = 0;
+}
+// Check if $uploadOk is set to 0 by an error
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file))
+    {
+        $stanje = $_POST['stanje'];
+        $velikost = $_POST['velikost'];
+        $lokacija = $_POST['lokacija'];
+        $najemnina = $_POST['najemnina'];
+        $opis = $_POST['opis'];
+        $zacetek = $_POST['zacetek'];
+        $konec = $_POST['konec'];
+        $pot = $_FILES['fileToUpload'];
 
 $sql = "INSERT INTO poslovni_prostori (stanje, velikost, lokacija, najemnina, opis, zacetek, konec, slika)
-VALUES ('$stanje','$velikost','$lokacija','$najemnina','$opis','$zacetek','$konec','$mysql_path')";
-
-
-if ($conn->query($sql) === TRUE) {
-    
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+VALUES ('$stanje','$velikost','$lokacija','$najemnina','$opis','$zacetek','$konec','$target_file')";
+                
+        $result = mysqli_query($conn, $sql);
+        
+        if($result)
+        {
+            echo 'Uspešno';
+        }
+        else
+        {
+            echo 'Napaka!';
+            var_dump($sql);
+        }
+        
+    } 
+    else 
+    {
+        echo "Sorry, there was an error uploading your file.";
+    }
 }
-
-$conn->close();
-}
-
-     //header( "Location: index.php" );
-
-?>
-

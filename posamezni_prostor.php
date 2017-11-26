608ede054087cfda521ae32bdded22df71546e07
ignjaticaco <aleksandar.ignjatic99@gmail.com>
2017-11-26 15:29:33 +0100

    Update posamezni_prostor.php

--- posamezni_prostor.php
+++ posamezni_prostor.php
@@ -1,8 +1,26 @@
 <?php
+session_start();
+include_once 'db.php'; ?>
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+<?php
 if (isset($_SESSION['id']))
 {
-    $sql = "SELECT * FROM poslovni_prostori WHERE id LIKE '$id'";
+    $sql = "SELECT * FROM poslovni_prostori WHERE id LIKE '1'";
+    $sql2 = "SELECT url FROM videoposnetki WHERE id_poslovni_prostor LIKE '1'";
         $result = mysqli_query($conn, $sql);
+        $qry = mysqli_query($conn, $sql2);
+        while ($row2 = mysqli_fetch_assoc($qry)) {
         while($row = mysqli_fetch_array($result))
         {
             $lokacija = $row['lokacija'];
@@ -15,16 +33,18 @@ if (isset($_SESSION['id']))
             $slika = $row['slika'];
             $opis = $row['opis'];
         }
-        echo "<table border=1>";
-            echo "<tr>
-                <td>$lokacija</td>
-                <td>$velikost</td>
-                <td>$najemnina</td>
-                <td>$prosto</td>
-                <td>$stanje</td>
-                <td>$zacetek</td>
-                <td>$konec</td>
-                <td>$slika</td>
-                <td>$opis</td>";
-        echo "</table>";
-}
+            echo "<div id=\"third\">
+                <div class=\"text3\">$lokacija</div>
+                <div class=\"text3\">$velikost</div>
+                <div class=\"text3\">$najemnina</div>
+                <div class=\"text3\">$prosto</div>
+                <div class=\"text3\">$stanje</div>
+                <div class=\"text3\">$zacetek</div>
+                <div class=\"text3\">$konec</div>
+                <div class=\"text3\">$slika</div>
+                <div class=\"text3\">$opis</div>
+            <div class=\"text3\"><iframe width=\"160\" height=\"120\"  frameborder=\"0\" allowfullscreen src=".$row2['url']."></iframe>
+        </div></div>";
+}}?>
+</body>
+</html>
